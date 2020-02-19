8cb25374053da346436e01c9013d70ed82d30ad0
PC <fdhfdh>
2020-02-19 10:48:26 +0200

    adding class-room

--- classes/models.py
+++ classes/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Classroom(models.Model):
 	subject = models.CharField(max_length=120)
-	grade = models.IntegerField()
+	grade = models.IntegerField(null=True,blank=True)
 	year = models.IntegerField()
+	teacher = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
 
 	def get_absolute_url(self):
 		return reverse('classroom-detail', kwargs={'classroom_id':self.id})
+class Student(models.Model):
+	Gender = (("M","Male"), ("F","Female"))
+	name = models.CharField(max_length=120)
+	dob = models.DateField()
+	gender = models.CharField(max_length=10,choices=Gender)
+	exam_grade = models.IntegerField()
+	classroom = models.ForeignKey(Classroom,on_delete=models.CASCADE)
